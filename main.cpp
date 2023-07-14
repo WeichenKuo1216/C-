d13f1676428015f360045f7d5924d51fab8307b7
WeichenKuo1216 <139571587+WeichenKuo1216@users.noreply.github.com>
2023-07-14 23:49:38 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,109 +1,56 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
+#include <list>
 
+using namespace std;
 
-// Function prototypes
-void playHangman();
-bool isGuessCorrect(const std::string& word, char guess, std::string& guessedWord);
-bool isGameOver(const std::string& word, const std::string& guessedWord, int attempts);
-bool playAgain();
+// Function prototype
+int josephus(int numPeople, int eliminationCount);
 
 int main() {
-    // Seed the random number generator
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    // Get the number of people and elimination count from the user
+    int numPeople, eliminationCount;
+    cout<<"Welcome to the Josephus Problem game"<<endl;
+    cout << "Enter the number of people: ";
+    cin >> numPeople;
+    cout << "Enter the elimination count: ";
+    cin >> eliminationCount;
 
-    // Game loop
-    do {
-        playHangman();
-    } while (playAgain());
+    // Calculate the winning participant
+    int winner = josephus(numPeople, eliminationCount);
+
+    // Display the winner
+    std::cout << "The winning participant is: " << winner << std::endl;
 
     return 0;
 }
 
-void playHangman() {
-    // Words to be guessed
-    std::vector<std::string> words = {
-        "apple",
-        "banana",
-        "cherry",
-        "durian",
-        "elderberry",
-        "fig",
-        "grape",
-        "honeydew",
-        "indigo",
-        "jackfruit"
-    };
+int josephus(int numPeople, int eliminationCount) {
+    // Create a linked list to represent the circle of people
+    std::list<int> circle;
+    for (int i = 1; i <= numPeople; ++i) {
+        circle.push_back(i);
+    }
 
-    // Select a random word from the list
-    std::string word = words[std::rand() % words.size()];
-    int wordLength = word.length();
-
-    // Initialize the guessed word with underscores
-    std::string guessedWord(wordLength, '_');
-
-    // Track the guessed letters
-    std::string guessedLetters;
-
-    // Number of attempts
-    int attempts = 6;
-
-    // Game loop
-    while (true) {
-        std::cout << "Word: " << guessedWord << std::endl;
-        std::cout << "Guessed letters: " << guessedLetters << std::endl;
-        std::cout << "Remaining attempts: " << attempts << std::endl;
-
-        // Get a letter guess from the player
-        char guess;
-        std::cout << "Enter a letter: ";
-        std::cin >> guess;
-
-        // Check if the guess is correct
-        if (isGuessCorrect(word, guess, guessedWord)) {
-            std::cout << "Correct guess!" << std::endl;
-        } else {
-            std::cout << "Incorrect guess!" << std::endl;
-            attempts--;
-        }
-
-        // Add the guessed letter to the list
-        guessedLetters += guess;
-
-        // Check if the game is over
-        if (isGameOver(word, guessedWord, attempts)) {
-            if (guessedWord == word) {
-                std::cout << "Congratulations! You guessed the word correctly: " << word << std::endl;
-            } else {
-                std::cout << "Game over! The word was: " << word << std::endl;
+    // Start the game
+    auto current = circle.begin();
+    while (circle.size() > 1) {
+        // Find the next person to be eliminated
+        for (int count = 1; count < eliminationCount; ++count) {
+            ++current;
+            if (current == circle.end()) {
+                current = circle.begin();
             }
-            break;
+        }
+
+        // Eliminate the current person
+        current = circle.erase(current);
+
+        // Wrap around if necessary
+        if (current == circle.end()) {
+            current = circle.begin();
         }
     }
-}
 
-bool isGuessCorrect(const std::string& word, char guess, std::string& guessedWord) {
-    bool correctGuess = false;
-    for (int i = 0; i < word.length(); i++) {
-        if (word[i] == guess) {
-            guessedWord[i] = guess;
-            correctGuess = true;
-        }
-    }
-    return correctGuess;
+    // Return the winning participant
+    return *current;
 }
-
-bool isGameOver(const std::string& word, const std::string& guessedWord, int attempts) {
-    return guessedWord == word || attempts <= 0;
-}
-
-bool playAgain() {
-    char choice;
-    std::cout << "Do you want to play again? (y/n): ";
-    std::cin >> choice;
-    return (choice == 'y' || choice == 'Y');
-}
-
